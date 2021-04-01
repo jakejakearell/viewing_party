1123f493a8a8987b390aea0302e1556e33c445ac
Gvieve <gnuebel13@gmail.com>
2021-03-31 19:34:03 -0600

    Create additional poros tests for film

--- spec/poros/film_spec.rb
+++ spec/poros/film_spec.rb
@@ -16,5 +16,28 @@ RSpec.describe Film do
       expect(film.id).to be_a(Integer)
       expect(film.vote_average).to be_a(Float)
     end
+
+    it 'should build a Film PORO based on input' do
+      page = 1
+
+      VCR.use_cassette('top_movie_data') do
+        @data_1 = MovieService.top_films(page)
+      end
+
+      @film_1 = Film.new(@data_1[:results][0])
+
+      VCR.use_cassette('movie_data') do
+        @data_2 = MovieService.movie_data(@film_1.id)
+      end
+
+      film_2 = Film.new(@data_2)
+      expect(film_2).to be_a(Film)
+      expect(film_2.title).to be_a(String)
+      expect(film_2.id).to be_a(Integer)
+      expect(film_2.vote_average).to be_a(Float)
+      expect(film_2.runtime).to be_a(Integer)
+      expect(film_2.genres.first).to be_a(Hash)
+      expect(film_2.summary).to be_a(String)
+    end
   end
 end
