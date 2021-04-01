2ec8482802e37bec505aac953bd46518d5831a43
Gvieve <gnuebel13@gmail.com>
2021-03-31 20:40:39 -0600

    Rubocop

--- app/facades/movies_facade.rb
+++ app/facades/movies_facade.rb
@@ -10,13 +10,13 @@ class MoviesFacade
   end
 
   def self.movie_search(query)
-      @movies = []
-      [1, 2].reduce([]) do |_memo, page|
-        MovieService.film_finder(page, query)[:results].each do |movie|
-          @movies << Film.new(movie)
-        end
-        @movies
+    @movies = []
+    [1, 2].reduce([]) do |_memo, page|
+      MovieService.film_finder(page, query)[:results].each do |movie|
+        @movies << Film.new(movie)
       end
+      @movies
+    end
   end
 
   def self.movie_info(id)
