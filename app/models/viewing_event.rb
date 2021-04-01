96ddf08b5c7d17d358bbef7d93d99a70754c4a80
Gvieve <gnuebel13@gmail.com>
2021-03-31 20:40:24 -0600

    Cleanup rubocop formatting

--- app/models/viewing_event.rb
+++ app/models/viewing_event.rb
@@ -4,19 +4,15 @@ class ViewingEvent < ApplicationRecord
   has_many :viewers
   has_many :users, through: :viewers
 
-  validates_presence_of [:duration, :start_date, :start_date_time]
+  validates :duration, :start_date, :start_date_time, presence: true
   validate :start_date_cant_be_in_the_past
   validate :duration_cant_be_less_than_movie_runtime
 
   def start_date_cant_be_in_the_past
-    if start_date && start_date < Date.today
-      errors.add(:start_date, "can't be in the past")
-    end
+    errors.add(:start_date, "can't be in the past") if start_date && start_date < Date.today
   end
 
   def duration_cant_be_less_than_movie_runtime
-    if duration && duration < Movie.find(movie_id).runtime
-      errors.add(:duration, "can't be less than movie runtime")
-    end
+    errors.add(:duration, "can't be less than movie runtime") if duration && duration < Movie.find(movie_id).runtime
   end
 end
